d25420076a9564db7538cc35ef0853bcbceb961f
SUVAM DAS <dsuvam954@gmail.com>
2026-03-16 15:39:37 +0530

    Enhance CreateMenu to manage multiple restaurants
    Added handling for multiple restaurants in CreateMenu component.

--- UI/create-menu/create-menu.ts
+++ UI/create-menu/create-menu.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { MenuService } from '../services/menu-service';
-import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { nonNegativePriceDirective } from '../directive/negativePriceValidator';
 import { UserModel } from '../model/UserModel';
 import { UserService } from '../services/user-service';
 import { RestaurantService } from '../services/restaurant-service';
+import { MenuService } from '../services/menu-service';
+
 @Component({
     selector: 'app-create-menu',
     imports: [FormsModule, CommonModule, nonNegativePriceDirective],
@@ -15,45 +17,60 @@ import { RestaurantService } from '../services/restaurant-service';
 })
 export class CreateMenu {
     currentUser !: UserModel;
-    restaurantId : any;
+    restaurantId: any;
     selectedFile: File | null = null;
-    constructor(private restaurantService:RestaurantService,private userService : UserService,private menuService: MenuService,private route:ActivatedRoute, private router: Router,private cdr : ChangeDetectorRef) { }
+    allrestaurant !: any;
+
+    constructor(
+        private restaurantService: RestaurantService,
+        private userService: UserService,
+        private menuService: MenuService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private cdr: ChangeDetectorRef
+    ) { }
+
     onFileSelected(event: any) {
         this.selectedFile = event.target.files[0];
     }
-  
-    ngOnInit(){
+
+    ngOnInit() {
+
         this.userService.getUserProfile().subscribe({
-         next:(response:any)=>{
-            console.log("Inside APP component ...",response);
-              this.currentUser = response.response.data;
-              this.cdr.detectChanges();
-              if(this.currentUser){
-                 this.restaurantService.getRestaurantByID(this.currentUser._id).subscribe({
-                     next : (response)=>{
-                        console.log("Restaurant Fetched ...",response);
-                        this.restaurantId = response.data[0]._id;
-                        console.log(this.restaurantId);
-                     },
-                     error:(err)=>{
-                        console.error("Error while fetching Restaurant Details...",err)
-                     }
-                })
-              }
-             
-              
-         },
-         error:(err:any)=>{
-           console.error("Error Occurred While Login ...",err);
-         }
-      })
+            next: (response: any) => {
+                console.log("Inside create Menu component ...", response);
+                this.currentUser = response.response.data;
+                console.log("Current User inside create  Menu...",this.currentUser);
+                this.cdr.detectChanges();
+                if (this.currentUser) {
+            this.restaurantService.getRestaurantByID(this.currentUser._id).subscribe({
+                next: (response: any) => {
+                    console.log("Restaurant Fetched Inside create Menu...", response);
+                    this.restaurantId = response.data[0]._id;
+                    this.allrestaurant = response.data;
+                    console.log(this.restaurantId);
+                    console.log(this.allrestaurant);
+                    this.cdr.detectChanges();
+                },
+                error: (err: any) => {
+                    console.error("Error while fetching Restaurant Details...", err)
+                }
+            })
+        }
+
+            },
+            error: (err: any) => {
+                console.error("Error Occurred While Login ...", err);
+            }
+        })
+
+        
     }
 
     handleSubmit(myForm: NgForm) {
         console.log("New Menu...");
         console.log(myForm.value);
         if (myForm.value) {
-            // this.menuService.addMenu(myForm.value,myForm.value.sectionName)
             const formData = new FormData();
             formData.append('itemName', myForm.value.itemName);
             formData.append('description', myForm.value.description);
@@ -61,21 +78,20 @@ export class CreateMenu {
             formData.append('price', myForm.value.price);
 
             if (this.selectedFile) {
-                formData.append('image', this.selectedFile); // field name must match backend
+                formData.append('image', this.selectedFile);
             }
-            console.log("FORM Data:" ,formData);
-            this.menuService.addMenu(this.restaurantId,formData).subscribe({
-                  next:(response)=>{
-                      console.log("Menu added successfully ...",response);
-                      alert("New Menu Add Succesfully...");
-                      this.router.navigate(['/']);
-                  },
-                  error:(err)=>{
-                     console.error("Error Occurred while creating Menu...",err);
-                  }
+            console.log("FORM Data:", formData);
+
+            this.menuService.addMenu(this.restaurantId, formData).subscribe({
+                next: (response: any) => {
+                    console.log("Menu added successfully ...", response);
+                    alert("New Menu Add Succesfully...");
+                    this.router.navigate(['/']);
+                },
+                error: (err: any) => {
+                    console.error("Error Occurred while creating Menu...", err);
+                }
             })
         }
-        
     }
-
 }
