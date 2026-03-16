b277bcaabb82906042867f551bcb5a4e7b1d0e54
SUVAM DAS <dsuvam954@gmail.com>
2026-03-16 15:46:32 +0530

    Add files via upload
    feedback component

---
+++ UI/feedback/feedback.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  imports: [],
+  templateUrl: './feedback.html',
+  styleUrl: './feedback.css',
+})
+export class Feedback {}
