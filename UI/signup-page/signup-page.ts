bbb0d8e7bb285b663cf50167cdfabc7a2c91d4d2
SUVAM DAS <dsuvam954@gmail.com>
2026-03-16 16:12:49 +0530

    Add Delhi to the list of Indian states in signup page

--- UI/signup-page/signup-page.ts
+++ UI/signup-page/signup-page.ts
@@ -22,14 +22,18 @@ import { zipCodeValidator } from '../directive/zipCode';
 export class SignupPage {
    // List of Indian States
   states: string[] = [
-    'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh',
+    'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh','Delhi',
     'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka',
     'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram',
     'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu',
     'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'
   ];
+
+  
   signupForm!: FormGroup;
   constructor( private fb: FormBuilder, private userservice: UserService, private router: Router) { }
+
+
   ngOnInit(): void {
     this.signupForm = this.fb.group(
       {
